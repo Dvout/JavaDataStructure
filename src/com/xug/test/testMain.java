8cb69a7df3c13f3015e1853ee38c409ef7669860
xugui <gui.xu@ly.com>
2019-11-06 20:40:29 +0800

    testmian

---
+++ src/com/xug/test/testMain.java
@@ -0,0 +1,12 @@
+package com.xug.test;
+
+/**
+ * @author gui.xu
+ * @Description
+ * @date 2019/11/6
+ */
+public class testMain {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
