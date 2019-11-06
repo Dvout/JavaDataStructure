8cf5e124fbfe3dc10965db43916192824c4d601d
xugui <gui.xu@ly.com>
2019-11-06 20:44:23 +0800

    test

--- src/com/xug/test/testMain.java
+++ src/com/xug/test/testMain.java
@@ -8,5 +8,6 @@ package com.xug.test;
 public class testMain {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
